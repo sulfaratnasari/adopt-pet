2113c8b061f96e70d0cd96a19006dde8e3dbb2f9
sulfaratnasari <sulfaratnasari@gmail.com>
2022-01-22 17:31:41 +0700

    first commit

---
+++ src/pages/Beranda/ImageSlide.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+
+const HeaderBeranda = () => {
+    return(
+        <Toolbar>
+            
+        </Toolbar>
+    )
+}
