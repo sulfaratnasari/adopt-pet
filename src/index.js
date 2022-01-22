abc2ddb628191e5c5bcad874f72936f5604401c9
putri <putrisanjiwanidewi@gmail.com>
2022-01-22 19:57:05 +0900

    setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import "antd/dist/antd.css"
 
 ReactDOM.render(
   <React.StrictMode>
