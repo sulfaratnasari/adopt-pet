7ad9012d2749dbaacf65e08151e1617f7631e042
putri <putrisanjiwanidewi@gmail.com>
2022-01-22 19:59:07 +0900

    add navbar and footer

---
+++ src/componets/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <ul>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Twitter</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/componets/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Layout,  Menu,Typography, Button } from "antd";
+import logo from '../Images/logo192.png'
+
+const { Header } = Layout;
+
+const Navbar=()=> {
+  return (
+    <Header style={{background:'#FFBD00',position: "fixed", zIndex: 1, width: "100%", display:'flex', flexDirection:'row'}}>
+      <div className="logo" style={{ marginRight:'1.5rem'}}><img src={logo} style={{width:'2rem'}}/></div>
+      <Menu mode="horizontal" style={{background:'#FFBD00'}}>
+        <Menu.Item key='1'>About Us</Menu.Item>
+        <Menu.Item key='2'>Rehome a Pet</Menu.Item>
+        <Menu.Item key='3'>Lost and Found</Menu.Item>
+        <Menu.Item key='4'>Pet Care and Health</Menu.Item>
+        <Menu.Item key='5'>Shop</Menu.Item>
+      </Menu>
+      <div className="login" style={{position: "absolute", right: '0', paddingRight:'1.5rem'}}>
+        <Button type="link" block>Login</Button>
+        
+      </div>
+    </Header>
+  );
+}
+
+export default Navbar;
