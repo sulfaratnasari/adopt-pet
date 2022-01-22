2d166cccf690fa9d91ba9414d8fa12add708fc71
sulfaratnasari <sulfaratnasari@gmail.com>
2022-01-22 20:42:05 +0700

    Login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,43 +1,55 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
-
+import { Form, Input, Button, Space, Card } from "antd";
 import "./Login.css";
+import logo from '../../componets/Images/logo.png'
 
 const Login = () => {
-  return (
-    <div className="login">
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        // onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
+	return (
+		<div className="login">
+			<Space direction="vertical">
+				<Card title="" className="card">
+					<div className="btn-register">
+						<Button type="text">Register</Button>
+					</div>
+					<img src={logo} className="logo-img-small" />
+					<Form
+						name="basic"
+						labelCol={{ span: 8 }}
+						wrapperCol={{ span: 16 }}
+						initialValues={{ remember: true }}
+						// onFinish={onFinish}
+						// onFinishFailed={onFinishFailed}
+						autoComplete="off"
+					>
+						<Form.Item
+							label="Email"
+							name="username"
+							labelCol={{ span: 24 }}
+							wrapperCol={{ span: 24 }}
+							rules={[{ required: true, message: "Please input your username!" }]}
+						>
+							<Input placeholder="enter your email" className="input" />
+						</Form.Item>
+						<Form.Item
+							label="Password"
+							name="password"
+							labelCol={{ span: 24 }}
+							wrapperCol={{ span: 24 }}
+							rules={[{ required: true, message: "Please input your password!" }]}
+						>
+							<Input.Password placeholder="enter your password" className="input" />
+						</Form.Item>
+						<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+							<Button htmlType="submit" className="btn-login">
+								Login
+							</Button>
+						</Form.Item>
+					</Form>
+				</Card>
+			</Space>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+		</div>
+	);
 };
 
 export default Login;
