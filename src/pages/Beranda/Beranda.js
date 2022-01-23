adb60f92c151d5f66cb4387a58ba4947ca886df3
putri <putrisanjiwanidewi@gmail.com>
2022-01-23 16:38:33 +0900

    change layout

--- src/pages/Beranda/Beranda.js
+++ src/pages/Beranda/Beranda.js
@@ -1,22 +1,20 @@
 import React from "react";
-import {Layout} from 'antd'
 
-import Navbar from "../../componets/Navbar/Navbar";
+import PageLayout from "../Layout.js/PageLayout";
 import ImageSlide from './ImageSlider/ImageSlide'
 import Feature from './Featured'
 import Search from './Search'
 import ImageData from "./ImageSlider/ImageData";
 
 
-const BerandaAdoptPets = () => {
 
+const BerandaAdoptPets = () => {
   return (
-    <Layout>
-      <Navbar/>
+    <PageLayout>
       <ImageSlide slider={ImageData}/>
       <Search/>
       <Feature/>
-    </Layout>
+    </PageLayout>
   );
 };
 
