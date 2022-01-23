5654c93ae475ffd00232985c0cc691e729a27b68
putri <putrisanjiwanidewi@gmail.com>
2022-01-23 16:38:01 +0900

    add route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import BerandaAdoptPets from '../src/pages/Beranda/Beranda'
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import BerandaAdoptPets from "../src/pages/Beranda/Beranda";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import PetDescription from "./pages/PetDesciption/PetDescription";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path = '/login' element={<Login />}/>
-		  <Route path = '/register' element={<Register />}/>
-          <Route path = '/' exact element= {<BerandaAdoptPets/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" exact element={<BerandaAdoptPets />} />
+          <Route path="/description" element={<PetDescription />}/>
         </Routes>
       </Router>
-      
     </div>
   );
 }
