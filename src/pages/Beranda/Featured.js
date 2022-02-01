7a832f82564a110eba3533b4f547c91ffa546f0c
putri <putrisanjiwanidewi@gmail.com>
2022-02-01 21:12:03 +0900

    routing

--- src/pages/Beranda/Featured.js
+++ src/pages/Beranda/Featured.js
@@ -1,18 +1,22 @@
 import React from "react";
 import dataPets from "./FeaturedData";
-
+import { useNavigate } from "react-router-dom";
 
 import './Featured.css'
 
 function Feature() {
+  const navigate = useNavigate()
+  const handleDescriptionPet = ()=>{
+    navigate('/description')
+
+  }
   return (
     <>
       <h3>Featured Pets</h3>
-      <hr/>
       <div className="feature">
         {dataPets.map((pets) => {
           return (
-            <div className="feature-item">
+            <div className="feature-item" onClick={handleDescriptionPet()}>
               <img src={pets.image} style={{width:'10rem', height:'11rem', borderRadius:'10px 10px 0px 0px'}}/>
               <h4>{pets.name}, <span>{pets.sex}</span></h4>
               <p>{pets.age} old</p>
